b68503f67bff6b0289214b5372889f59da8c388e
syyad khan <syyadkhan@yahoo.com>
2019-08-31 14:36:02 -0400

    Commit of scss logo basic style

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "./App.css";
+import "./app.scss";
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Testing</h1>
+        <Header />
       </header>
     </div>
   );
