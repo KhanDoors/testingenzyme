cfd72a981a82131e654b7dad5203e71b30e34208
syyad khan <syyadkhan@yahoo.com>
2019-09-01 12:34:47 -0400

    proptypes added with tests

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,23 @@ import "./app.scss";
 import Header from "./components/header";
 import Headline from "./components/headline";
 
+const tempArr = [
+  {
+    fName: "Joe",
+    lName: "Shmo",
+    email: "joeshmo@whocares.com",
+    age: 99,
+    onlineStatus: false
+  }
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
         <section className="main">
-          <Headline header="Posts" desc="Click the Button" />
+          <Headline header="Posts" desc="Click the Button" tempArr={tempArr} />
         </section>
       </header>
     </div>
