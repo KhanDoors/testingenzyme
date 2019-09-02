62b9549ba87d540bc0a1abf55c687e77507b8a06
syyad khan <syyadkhan@yahoo.com>
2019-09-02 14:03:10 -0400

    button apiget integration testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import "./app.scss";
 import Header from "./components/header";
 import Headline from "./components/headline";
+import SharedButton from "./components/button";
+import ListItem from "./components/listItem";
+import { connect } from "react-redux";
+import { fetchPosts } from "./actions/index";
 
 const tempArr = [
   {
@@ -13,17 +17,56 @@ const tempArr = [
   }
 ];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <section className="main">
-          <Headline header="Posts" desc="Click the Button" tempArr={tempArr} />
-        </section>
-      </header>
-    </div>
-  );
+class App extends Component {
+  fetch = () => {
+    this.props.fetchPosts();
+  };
+
+  render() {
+    const { posts } = this.props;
+
+    const configButton = {
+      buttonText: "Get Posts",
+      emitEvent: this.fetch
+    };
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Header />
+          <section className="main">
+            <Headline
+              header="Posts"
+              desc="Click the Button"
+              tempArr={tempArr}
+            />
+            <SharedButton {...configButton} />
+            {posts.length > 0 && (
+              <div>
+                {posts.map((post, index) => {
+                  const { title, body } = post;
+                  const configListItem = {
+                    title,
+                    desc: body
+                  };
+                  return <ListItem key={index} {...configListItem} />;
+                })}
+              </div>
+            )}
+          </section>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(App);
